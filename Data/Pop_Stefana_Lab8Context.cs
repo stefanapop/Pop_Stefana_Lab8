b1ec6ef32d261f000f4a4072163ccc71c03f4358
Stefana <po.st0165089s@stud.portalid.ubbcluj.ro>
2020-11-21 08:41:56 +0200

    Add project files.

---
+++ Data/Pop_Stefana_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Stefana_Lab8.Models;
+
+namespace Pop_Stefana_Lab8.Data
+{
+    public class Pop_Stefana_Lab8Context : DbContext
+    {
+        public Pop_Stefana_Lab8Context (DbContextOptions<Pop_Stefana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Stefana_Lab8.Models.Book> Book { get; set; }
+    }
+}
